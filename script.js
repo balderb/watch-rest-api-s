8d437a9adf324f6b6b198ca5fc3ddbd22311fd08
Balder Bongaerts <59473814+balderb@users.noreply.github.com>
2020-07-29 01:08:58 +0200

    adding last changes

--- script.js
+++ script.js
@@ -1,15 +1,8 @@
 let map;
 
 function initMap() {
-    // map options + new map
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 50.85045, lng: 4.34878},
-    zoom: 15
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8
   });
 }
-
-var marker = new google.maps.Marker({
-    position: {lat:50.871220, lng:4.083370},
-    map:map
-});
-    
